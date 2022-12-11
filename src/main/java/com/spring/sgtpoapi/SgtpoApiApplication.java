a630dd04d2a66e6c68128d95604e2164af14eb02
FellipeToledo <fellipetoledo.dev@gmail.com>
2022-12-11 17:42:45 -0300

    Initial commit

---
+++ src/main/java/com/spring/sgtpoapi/SgtpoApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sgtpoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtpoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgtpoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sgtpoapi/SgtpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sgtpoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
