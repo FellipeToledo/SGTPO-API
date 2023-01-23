94048994eaf9c409fea24c6281c02bf1a430c4d3
fellipe <fellipetoledo.dev@gmail.com>
2023-01-23 11:24:34 -0300

    refactored repository

--- src/main/java/com/spring/sgtpoapi/Repository/ConsortiumRepository.java
+++ src/main/java/com/spring/sgtpoapi/Repository/ConsortiumRepository.java
@@ -1,11 +1,9 @@
 package com.spring.sgtpoapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.spring.sgtpoapi.entity.ConsortiumEntity;
 
-@Repository
 public interface ConsortiumRepository extends JpaRepository<ConsortiumEntity, Long>{
 
 }
